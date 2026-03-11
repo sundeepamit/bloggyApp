d67882f58b34d5a33215c315de0cd9d39a19929a
sundeepamit <sundeepamitmaurya@gmail.com>
2026-03-11 11:10:19 +0530

    feat: initial commit

---
+++ components/web/Footer.tsx
@@ -0,0 +1,6 @@
+// Named export
+export function Footer() {
+    return <div>
+        <h1>Footer goes here</h1>
+    </div>
+}
---
+++ components/web/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export function Navbar() {
+    return (
+        <nav className="py-5 flex items-center justify-between">
+            <div className="flex items-center gap-6">
+                <Link href={"/"}>
+                    <h1 className="text-3xl font-semibold">Blo<span className="text-blue-500">gyy</span></h1>
+                </Link>
+                <div className="sm:flex gap-6 items-center hidden">
+                    <Link href="/" className="text-sm font-medium hover:text-blue-500 transition-color">Home</Link>
+                    <Link href="/dashboard" className="text-sm font-medium hover:text-blue-500 transition-color">Dashboard</Link>
+
+                </div>
+            </div>
+            <div className="flex gap-4 items-center">
+
+            </div>
+        </nav>
+    )
+}
