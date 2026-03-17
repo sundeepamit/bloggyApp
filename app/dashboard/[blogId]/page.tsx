56bb89b499ea5ea331f0d7fc0586f9fcefff9db3
sundeepamit <sundeepamitmaurya@gmail.com>
2026-03-17 13:28:54 +0530

    some bug

--- app/dashboard/[blogId]/page.tsx
+++ app/dashboard/[blogId]/page.tsx
@@ -1,19 +1,27 @@
 import { getPostById } from "@/app/actions"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { Heart } from "lucide-react"
 type BlogProp = {
     params: Promise<{ blogId: string }>
 }
 
 export default async function BlogPostById(props: BlogProp) {
     const { blogId } = await props.params
-
     const post = await getPostById(blogId)
     // console.log(post)
     return (
         <div className="mt-3">
             <main>
                 <h1 className="text-4xl font-bold text-shadow-2xs antialiased">{post.title} </h1>
+                <div className="flex items-center justify-between">
+                    <span className="mt-4 inline-block text-gray-500">Created At: {post.createdAt.toLocaleDateString()}
+                    </span>
+                    {/* fix :like logic  */}
+                    <span className="mr-7"><Heart className="w-5 h-5 " /></span>
+                </div>
+
+
                 <img src={post.imageUrl} alt="image of post" className="w-3xl rounded-2xl mt-8 shadow-xl" />
                 <p className="mt-7 mb-6 text-xl text-gray-700 antialiased tracking-wide">{post.content}</p>
                 <Button variant={"default"} className="text-2xl p-6" ><Link href={"/dashboard"}>Back</Link></Button>
